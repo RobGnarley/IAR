610d74102097c76e649558cef40f9dbe7144c675
Mattias Appelgren <mattiasappel@gmail.com>
2015-10-07 16:30:48 +0100

    clean up

--- stupid_control.m
+++ stupid_control.m
@@ -1,17 +1,23 @@
+% Initialise global variables
 ideal_distance = 300;
-K = 0.001;
 speed = 10;
 vleft = speed;
 turn_speed = 5;
 error = 0;
+
+% Main control loop 
 while true
     %pause(0.1);
+
     percepts = readIR(s);
-    if size(percepts, 2) == 8
+    % Check if the percept input is in the correct form.
+    % Should reduce the amount of crashes due to corrupt serial communication
+    if size(percepts, 2) == 8 
     front_average = (percepts(4) + percepts(3))/2;
     left_average = (percepts(1) + percepts(2) + percepts(8))/3;
     right_average = (percepts(5) + percepts(6) + percepts(7))/3;
     
+    % Object detection layer - layer 1
     if front_average > 100
         if left_average < right_average % turn left
             while percepts(5) > 100
@@ -26,7 +32,8 @@ while true
             end
         end
     else
-       if percepts(6) > 100
+      % Wall following layer - layer 2
+       if percepts(6) > 100 % If a wall is detected on the right
           
           old_vleft = vleft;
           old_error = error;
@@ -43,7 +50,7 @@ while true
                   go(s, speed)
               end
           end
-       else if percepts(1) > 100
+       else if percepts(1) > 100 % If a wall is detected on the left
           old_vleft = vleft;
           old_error = error;
           error = ideal_distance - percepts(1);
@@ -59,7 +66,7 @@ while true
                   go(s, speed)
               end
           end
-           else
+           else % If no walls nor objects found - go straight forwards
             vleft = speed;
             go(s, speed);
            end
